a088b8e4d4819a1729e78868508a43ab570a2618
yunzhaoyu2050 <yunzhaoyu@outlook.com>
2021-12-19 09:12:35 +0000

    update config.simple.php

--- config.simple.php
+++ config.simple.php
@@ -1,7 +1,7 @@
 <?php
 	// 默认读取同级目录
 	$siteinfo = array(
-		"site_name"	=>	"<i class='fa fa-file-code-o' aria-hidden='true'></i> KeySi File Server",
+		"site_name"	=>	"KeySi File Server",
 		"title"		=>	"KeySi File Server",
 		"keywords"	=>	"",
 		"description"	=>	""
@@ -27,7 +27,7 @@
 	$config = array(
 		// thedir为需要读取的目录，如:/data/wwwroot/soft.xiaoz.org，仅将zdir放在子目录的情况下需要配置此项，其它请勿配置此选项
 		// "thedir"	=>	$thedir,
-		"thedir"	=>	'files/',
+		"thedir"	=>	'/www/admin/192.168.0.24_8084/wwwroot/files/',
 		"allowip"	=>	array(
 			//"0.0.0.0",	// 注意设置为0.0.0.0则不限制IP，更多说明请参考帮助文档：https://doc.xiaoz.me/#/zdir/
 			"::1",
